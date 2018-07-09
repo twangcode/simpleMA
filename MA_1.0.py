1f00b00d4d64f6fdac49513940b12b830404e013
twangcode <40374008+twangcode@users.noreply.github.com>
2018-07-09 12:03:33 -0500

    Version_1.0.py

--- MA_1.0.py
+++ MA_1.0.py
@@ -35,40 +35,83 @@ def get_spread(name):
 def calculate_MA(data):
 	data['MA'] = data['Spread'].rolling(window=48*60).mean()
 	data['STD'] = data['Spread'].rolling(window=48*60).std()
-	# data['Upper_Band'] = data['MA'] + 2 * data['STD']
-	# data['Lower_Band'] = data['MA'] - 2 * data['STD']
+	return data
+
+def Trading_Signal_Long(data, entry=2.0, exit=0.5):
+	# Check when price is above or below our entry or exit level
+	# *** Important! This is not the final trading signal. This only tells us when price is below or above our entry or exit level. 
+	data['Init_Long'] = np.where(data['Spread'] < (data['MA'] - data['STD'] * entry), 1, 0)
+	data['Takeoff_Long'] = np.where(data['Spread'] > (data['MA'] - data['STD'] * entry * exit), -1, 0)
+	
+	
+	result = data[(data['Init_Long'] + data['Takeoff_Long']) != 0].copy()
+	trade = result[((result['Init_Long'] - result['Init_Long'].shift(1)) == 1) \
+				 | ((result['Takeoff_Long'] - result['Takeoff_Long'].shift(1)) == -1)].copy()
+	data=data.join(trade[['Init_Long', 'Takeoff_Long']], how='left', lsuffix='_1')
+	data['Trade'] = data['Init_Long'] + data['Takeoff_Long']
+	return data
+
+def Trading_Signal_Short(data, entry=2.0, exit=0.5):
+	data['Init_Short'] = np.where(data['Spread'] > (data['MA'] + data['STD'] * entry), -1, 0)
+	data['Takeoff_Short'] = np.where(data['Spread'] < (data['MA'] + data['STD'] * entry * exit), 1, 0)
+
+	result = data[(data['Init_Short'] + data['Takeoff_Short']) != 0].copy()
+	trade = result[((result['Init_Short'] - result['Init_Short'].shift(1)) == -1) \
+				 | ((result['Takeoff_Short'] - result['Takeoff_Short'].shift(1)) == 1)].copy()
+	data=data.join(trade[['Init_Short', 'Takeoff_Short']], how='left', lsuffix='_1')
+	data['Trade'] = data['Init_Short'] + data['Takeoff_Short']
+
 	return data
 
 def Trading_Signal(data, entry=2.0, exit=0.5):
-	data['Init_Long'] = np.where(data['Spread'] < (data['MA'] - data['STD'] * entry), 1, 0)
-	data['Takeoff_Long'] = np.where(data['Spread'] > (data['MA'] - data['STD'] * entry * exit), 1, 0)
-	result = data[(data['Init_Long'] + data['Takeoff_Long']) != 0].copy()
-	result['Init_Long_1'] = result['Init_Long'] - result['Init_Long'].shift(1)
-	result['Takeoff_Long_1'] = result['Takeoff_Long'] - result['Takeoff_Long'].shift(1)
-	# data['temp'] = data['Init_Long'] + data['Takeoff_Long']
-	# data['Position'] = np.trunc((data['MA'] - data['Spread']) / data['STD'])
-	# data['Position'] = np.where(data['Position'].isnull(), 0, data['Position'])
-	# data['Trade'] = data['Position'] - data['Position'].shift(1)
-	# data['Price'] = data['Trade'] * data['Spread']
-	# data['CumPrice'] = data['Price'].cumsum()
-	# data['Value'] = data['Spread'] * data['Position']
-	# data['PnL'] = data['Value'] - data['CumPrice']
-	# data['Position'] = np.where(data['Spread'] > data['Upper_Band'], 1, 0)
-	# data['Position'] = np.where(data['Spread'] < data['Lower_Band'], -1, data['Position'])
+	if {'Spread', 'MA', 'STD'}.issubset(data.columns):
+		Trade = data[['Spread', 'MA', 'STD']].copy()
+		Trade['Trigger_Long'] = np.where(Trade['Spread'] < (Trade['MA'] - Trade['STD'] * entry), 1, 0)
+		Trade['Trigger_Takeoff_Long'] = np.where(Trade['Spread'] > (Trade['MA'] - Trade['STD'] * entry * exit), 1, 0)
+		Trade['Trigger_Short'] = np.where(Trade['Spread'] > (Trade['MA'] + Trade['STD'] * entry), 1, 0)
+		Trade['Trigger_Takeoff_Short'] = np.where(Trade['Spread'] < (Trade['MA'] + Trade['STD'] * entry * exit), 1, 0)
+
+		Trade['Position'] = 0
+		flag = 0
+		for i in Trade.index:
+			if Trade.at[i, 'Trigger_Long'] == 1:
+				flag = 1
+				Trade.at[i, 'Position'] = 1
+			elif Trade.at[i, 'Trigger_Short'] == 1:
+				flag = -1
+				Trade.at[i, 'Position'] = -1
+			elif (Trade.at[i, 'Trigger_Takeoff_Long'] + Trade.at[i, 'Trigger_Takeoff_Short']) == 1:
+				Trade.at[i, 'Position'] = flag
+			elif (Trade.at[i, 'Trigger_Takeoff_Long'] + Trade.at[i, 'Trigger_Takeoff_Short']) == 2:
+				Trade.at[i, 'Position'] = 0
+				flag = 0
+		return Trade
+
+
+
+def Back_Test(data):
+	result = data[['Spread','Position']].copy()
+	result['Trade'] = result['Position'] - result['Position'].shift(1)
+	# result['Position'] = result['Trade'].cumsum()
+	result['Price'] = result['Trade'] * result['Spread']
+	result['CumPrice'] = result['Price'].cumsum()
+	result['Value'] = result['Spread'] * result['Position']
+	result['PnL'] = result['Value'] - result['CumPrice']
 	return result
+
 
 def test_run():
 	data = get_spread('R-ZN+.3*B6')
 	data = calculate_MA(data)
-	result = Trading_Signal(data)
-	# data[['Spread', 'Upper_Band', 'Lower_Band']].plot()
-	
-	result[(result['Init_Long_1'] == 1) | (result['Takeoff_Long_1'] == 1)].to_csv('test.csv')
-	# data[['Spread','MA']].plot()
-	# data[['Spread', 'Position', 'Trade', 'Price', 'CumPrice', 'Value', 'PnL']].to_csv('test_1.csv')
-	# data[data['Trade'] != 0].to_csv('test.csv')
-	# data['PnL'].plot()
-	# print (data['Position'] - data['Position'].shift(1)).value_counts()
+	# trade = Trading_Signal_Long(data)
+	# trade = Trading_Signal_Short(data)
+
+	trade = Trading_Signal(data)
+	trade.to_csv('test.csv')
+
+	result = Back_Test(trade)
+	result.to_csv('test.csv')
+	result['PnL'].plot()
 	plt.show()
 
 if __name__ == '__main__':
