aae4e2dfaef4511fe6a3a915059ccbf0a10c6202
twangcode <40374008+twangcode@users.noreply.github.com>
2018-07-26 11:47:52 -0500

    minor update

--- backtest.py
+++ backtest.py
@@ -1,6 +1,7 @@
 import tuotrading as tt 
 from spread import * 
 from dynamic_EE import *
+from datetime import datetime
 import matplotlib.pyplot as plt
 import numpy as np 
 import pandas as pd
@@ -11,10 +12,7 @@ entries = np.linspace(.5, 4, 1)
 exits = np.linspace(.1, 1, 1)
 threshold = 35
 
-
-
-def main():
-	print 'start...'
+def find_opt_entry_exit(spread_name, base_dir, entries, exits, threshold):
 	spread = Spread(spread_name, base_dir)
 	pnl = pd.DataFrame(index=spread.data.index)
 	for entry in entries:
@@ -23,7 +21,11 @@ def main():
 			name = 'Entry='+str(entry)+','+'Exit='+str(exit)
 			pnl[name] = strt.data['cumPnL']
 	pnl.plot()
-	plt.show()
+	plt.show() 
+
+def main():
+	print 'start...'
+	find_opt_entry_exit(spread_name, base_dir, entries, exits, threshold)
 	
 if __name__ == '__main__':
 	main()
