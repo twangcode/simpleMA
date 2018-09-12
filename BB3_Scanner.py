e9212ad5be679f3e0714660e57b986f85faded50
twangcode <40374008+twangcode@users.noreply.github.com>
2018-09-12 11:56:16 -0500

    update

--- BB3_Scanner.py
+++ BB3_Scanner.py
@@ -1,3 +1,4 @@
+import os
 import tuotrading as tt 
 from datetime import date, timedelta
 import numpy as np 
@@ -39,7 +40,7 @@ def bb3_scanner(BB3_list_filename, base_dir, start_date, end_date, entry=2.0, ex
 
 		result[spread_name] = sharpeRatio
 
-		print spread_name, sharpeRatio
+		# print spread_name, sharpeRatio
 
 	df_Sharpe = pd.DataFrame.from_dict({'Sharpe':result}).sort_values(by='Sharpe', ascending=False)
 
@@ -51,12 +52,23 @@ def bb3_scanner_delta_days(BB3_list_filename, base_dir, end_date, delta_days, en
 	return df_Sharpe
 
 def main():
-	BB3_list_filename = 'data/BB3/BB3_D.csv'
 	end_date = date.today()
-	# start_date = end_date - timedelta(days=60)
-	start_date_2 = end_date - timedelta(days=30)
+	start_date = end_date - timedelta(days=30)
+	start_date_2 = end_date - timedelta(days=10)
+
 	base_dir = 'data/data_2018'
-	print bb3_scanner(BB3_list_filename, base_dir, start_date_2, end_date)
+	bb3_list_folder = 'data/BB3'
+	bb3_file_list = os.listdir(bb3_list_folder)
+	for file in bb3_file_list:
+		if file.endswith('.csv'):
+			SR_30d = bb3_scanner(os.path.join(bb3_list_folder,file), base_dir, start_date, end_date)
+			SR_30d = SR_30d.rename(columns={'Sharpe':'Sharpe_30'})
+			SR_10d = bb3_scanner(os.path.join(bb3_list_folder,file), base_dir, start_date_2, end_date)
+			SR_10d = SR_10d.rename(columns={'Sharpe':'Sharpe_10'})
+			SR = SR_10d.join(SR_30d)
+			SR = SR.sort_values(by='Sharpe_10', ascending=False)
+			SR.to_csv('data/{}.csv'.format(file[:-4]+'_log'))
+			print 'finished '+'data/{}.csv'.format(file[:-4]+'_log')
 	
 
 def main_2():
